916930587b3b2ab449fccbc86905a784031cea31
Francisco José Rivero <francisco.rivero@local.eurecat.org>
2026-05-15 11:27:26 +0200

    responsive-blur-fix-images-added

--- eltaller/app/providers.tsx
+++ eltaller/app/providers.tsx
@@ -4,23 +4,14 @@ import { ThemeProvider } from "next-themes";
 import { useEffect, useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  // Evitar errores de hidratación asegurándonos de que el componente esté montado
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
     <ThemeProvider 
       attribute="data-theme" 
       defaultTheme="eltaller" 
       enableSystem={true}
       themes={["eltaller", "eltallerdark"]}
+      disableTransitionOnChange
+      enableColorScheme={false}
     >
       {children}
     </ThemeProvider>
